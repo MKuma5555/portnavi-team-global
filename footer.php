5841ad0ec7367783e115eca02a6ba1fcf4717cc0
mkumachan <camysblossoms@gmail.com>
2025-09-07 17:52:17 +1000

    基本的なコード

---
+++ footer.php
@@ -0,0 +1,10 @@
+
+<footer class="wrapper">
+      <p class="copyright">&copy; PortNavi</p>
+    </footer>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="/js/main.js"></script>
+    <?php wp_footer();?>
+  </body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,36 @@
+<aside class="side-nav">
+      <ul>
+        <li class="file-tab" role="presentation" tabindex="0">
+          <a href="<?php echo esc_url(home_url());?>" role="tab">WEBサイト</a>
+          <div class="index-panel" role="tabpanel" aria-hidden="false">
+            <ul>
+              <li><a href="<?php echo esc_url(home_url());?>">1</a></li>
+              <li><a href="<?php echo esc_url(home_url());?>">2</a></li>
+              <li><a href="<?php echo esc_url(home_url());?>">3</a></li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="file-tab" role="presentation" tabindex="0">
+          <a href="<?php echo esc_url(home_url());?>" role="tab">デザイン</a>
+          <div class="index-panel" role="tabpanel" aria-hidden="true">
+            <ul>
+              <li><a href="<?php echo esc_url(home_url());?>">4</a></li>
+              <li><a href="<?php echo esc_url(home_url());?>">5</a></li>
+              <li><a href="<?php echo esc_url(home_url());?>">6</a></li>
+            </ul>
+          </div>
+        </li>
+
+        <li class="file-tab" role="presentation" tabindex="0">
+          <a href="<?php echo esc_url(home_url());?>" role="tab">カラー別</a>
+          <div class="index-panel" role="tabpanel" aria-hidden="true">
+            <ul>
+              <li><a href="<?php echo esc_url(home_url());?>">7</a></li>
+              <li><a href="<?php echo esc_url(home_url());?>">8</a></li>
+              <li><a href="<?php echo esc_url(home_url());?>">9</a></li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </aside>
